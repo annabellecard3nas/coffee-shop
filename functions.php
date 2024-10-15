8f3fea8f26515596e2cda89d83e3a56fff6ae2ba
annabellecard3nas <119866562+annabellecard3nas@users.noreply.github.com>
2024-10-15 15:58:58 -0400

    debut du coffee shop

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php 
+
+function coffeeShop_files(){
+    /*
+    appeler les script javaScript 
+    (arra('jquery') c'est  pour quand ton javascript depend de d'autre files javaScript)
+    Null si ton file javaScript le depend de aucun autre dossier javaScript
+
+    le dernier argument  sert a savoir si tu veux que sa load au debut ou a la fin du body 
+    (true=charge en bas de la page au lieu d'en haut ce qui est meilleur pour la performance en general)
+    */
+    wp_enqueue_script('buger_button', get_theme_file_uri('/build/index.js'), array('jquery'),'1.0', true);
+
+
+    //pour appeler les styles
+    wp_enqueue_style('font_google_Lato','//fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+    wp_enqueue_style('font_google_EB_Garamond','//fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap');
+    wp_enqueue_style('font_google_montserrat','//fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+    wp_enqueue_style('coffeShop_main_styles', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts','coffeeShop_files');
+
+function ajout_au_theme(){
+    add_theme_support( 'post-thumbnails' ); 
+	add_theme_support( 'custom-logo');
+    add_theme_support( 'menus' );
+}
+add_action('after_setup_theme','ajout_au_theme');
+
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+    <main>
+        <section class="hautDePage">
+            <div>
+            <h1><?php echo get_bloginfo('name'); ?></h1>
+            <p>Where every coffee is crafted with passion, and served with love to bring you joy, and awaken your senses</p>
+            <p>So just sip and relax...</p>
+            <button class="main_button">Shop</button>
+            </div>
+            
+        </section>
+
+        <section class="milieu">
+
+        </section>
+    </main>
+
+
+
+<?php get_footer(); ?>    
+    
+
